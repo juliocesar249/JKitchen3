daf79c602b56468dbdaaa6dc530c4507802dba09
juliocesar249 <jc2493718@gmail.com>
2025-09-22 11:54:10 -0300

    modificado: passagem de props e estilização consertada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
+import { ChefHatIcon } from "lucide-react";
 import { Titulo } from "../Titulo";
 import style from "./styles.module.css";
 
 export function Header() {
   return (
     <header className={style.header}>
-      <Titulo/>
+      <Titulo type="main">JKitchen<ChefHatIcon size={116}/></Titulo>
     </header>
   )
 }
