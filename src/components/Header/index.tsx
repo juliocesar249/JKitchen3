f4fe6a7f74022d0195dbedb20071b2acfceadfaf
juliocesar249 <jc2493718@gmail.com>
2025-09-22 14:44:55 -0300

    adicionado: barra de navegação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
 import { ChefHatIcon } from "lucide-react";
 import { Title } from "../Title";
 import style from "./styles.module.css";
+import { NavBar } from "../NavBar";
 
 export function Header() {
   return (
     <header className={style.header}>
       <Title type="main">JKitchen<ChefHatIcon size={116}/></Title>
+      <NavBar/>
     </header>
   )
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,6 +2,11 @@ import style from "./styles.module.css";
 
 export function NavBar() {
   return (
-    <nav></nav>
+    <nav className={style.nav}>
+      <div className={style.linksWrapper}>
+        <a href="#" className={style.linkSelected}>Cardápio</a>
+        <a href="#">Contato</a>
+      </div>
+    </nav>
   )
 }
