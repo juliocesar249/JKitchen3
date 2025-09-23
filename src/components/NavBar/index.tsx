2a9205714e32a6c2d50ca8e20965b5648a5eeecd
juliocesar249 <jc2493718@gmail.com>
2025-09-22 21:45:29 -0300

    adicionado: botão de alteração de tema e lógica

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,11 +1,30 @@
+import { MoonIcon } from "lucide-react";
 import style from "./styles.module.css";
+import { useEffect, useState } from "react";
+
+type AllowedThemes = "dark" | "light"
 
 export function NavBar() {
+  const [theme, setTheme] = useState<AllowedThemes>(() => {
+    const storagedTheme = localStorage.getItem("theme") as AllowedThemes || "light";
+    return storagedTheme;
+  });
+
+  function changeTheme() {
+    setTheme(prev => prev === "dark" ? "light" : "dark");
+  }
+
+  useEffect(() => {
+    
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme])
+
   return (
     <nav className={style.nav}>
       <div className={style.linksWrapper}>
-        <a href="#" className={style.linkSelected}>Cardápio</a>
-        <a href="#">Contato</a>
+        <a href="#" title="Cardápio" aria-label="Cardápio de items para montar o prato" className={style.linkSelected}>Cardápio</a>
+        <a href="#" title="Contato" aria-label="Informações de contato">Contato</a>
+        <a href="#" onClick={changeTheme} title="Tema" aria-label="Alternear entre tema claro e escuro"><MoonIcon size={40}/></a>
       </div>
     </nav>
   )
